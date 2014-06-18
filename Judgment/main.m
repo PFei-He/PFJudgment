3623469e1c3cf2fb98ff64d909bd3d6e925e190d
PFei_He <user@userdeMac-mini-2.local>
2014-06-18 15:50:29 +0800

    Initial Commit

---
+++ Judgment/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Judgment
+//
+//  Created by PFei_He on 14-6-18.
+//  Copyright (c) 2014年 PFei_He. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
